5ae874c599c2f3cd34edd9120acb4d14e1f1ae1a
Endy Muhardin <endy.muhardin@gmail.com>
2014-03-20 20:48:21 +0700

    test method kurang

--- sesi-03/sample-code/belajar-junit/src/main/java/com/muhardin/endy/belajar/Kalkulator.java
+++ sesi-03/sample-code/belajar-junit/src/main/java/com/muhardin/endy/belajar/Kalkulator.java
@@ -4,4 +4,8 @@ public class Kalkulator {
     public Integer tambah(Integer x, Integer y){
         return x + y;
     }
+	
+	public Integer kurang(Integer x, Integer y) {
+		return x - y;
+	}
 }
--- sesi-03/sample-code/belajar-junit/src/test/java/com/muhardin/endy/belajar/KalkulatorTest.java
+++ sesi-03/sample-code/belajar-junit/src/test/java/com/muhardin/endy/belajar/KalkulatorTest.java
@@ -10,4 +10,11 @@ public class KalkulatorTest {
         Integer hasil = k.tambah(3,4);
 		assertEquals(new Integer(7), hasil);
 	}
+	
+	@Test
+	public void testKurang(){
+		Kalkulator k = new Kalkulator();
+		Integer hasil = k.kurang(3,4);
+		assertEquals(new Integer(-1), hasil);
+	}
 }
