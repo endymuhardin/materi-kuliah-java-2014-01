8741b92f8e68d75b0f8d65df1c43da9352a461c9
Endy Muhardin <endy.muhardin@gmail.com>
2014-04-03 20:07:57 +0700

    contoh kode program insert ke database

--- sesi-04/belajar-jdbc/src/main/java/com/muhardin/endy/belajar/App.java
+++ sesi-04/belajar-jdbc/src/main/java/com/muhardin/endy/belajar/App.java
@@ -1,13 +1,30 @@
 package com.muhardin.endy.belajar;
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+import java.sql.*;
+
+public class App {
+    public static void main( String[] args ) throws Exception {
+		// 0. Variabel Koneksi
+		String dbDriver = "com.mysql.jdbc.Driver";
+		String dbUrl = "jdbc:mysql://localhost/belajarjava";
+		String dbUsername = "root";
+		String dbPassword = "";
+		
+		// 1. Load library database
+		Class.forName(dbDriver);
+		
+		// 2. Connect ke database
+		Connection koneksi = DriverManager.getConnection(dbUrl, dbUsername, dbPassword);
+		
+		// 3. Insert Data
+		String sql = "insert into t_user ";
+		sql += "(username, email, name) values ";
+		sql += "('endy', 'endy.muhardin@gmail.com', 'Endy Muhardin')";
+		
+		PreparedStatement stm = koneksi.prepareStatement(sql);
+		stm.executeUpdate();
+		
+		// 4. Disconnect
+		koneksi.close();
     }
 }
