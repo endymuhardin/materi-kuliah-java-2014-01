14c6296caf5d4b75fb489907f1d54089d19bb4ab
Endy Muhardin <endy.muhardin@gmail.com>
2014-05-22 20:00:17 +0700

    contoh hello world dengan js

---
+++ sesi-07/sample-code/helloworld.js
@@ -0,0 +1,9 @@
+console.log("Hello World");
+
+var buah = ["Pepaya", "Pisang", "Mangga", "Jambu"];
+
+for(var i=0; i<buah.length; i++){
+    console.log(buah[i]);
+}
+
+// Cara menjalankan : nodejs helloworld.js
