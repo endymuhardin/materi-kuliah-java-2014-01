c9a4665e48d0c9570af14eaaea0f4beb1009b2f3
Endy Muhardin <endy.muhardin@gmail.com>
2014-06-05 20:57:24 +0700

    implement form edit

--- sesi-08/sample-code/client-side/app/scripts/controllers/mahasiswa.js
+++ sesi-08/sample-code/client-side/app/scripts/controllers/mahasiswa.js
@@ -2,34 +2,26 @@
 
 angular.module('clientSideApp')
   .controller('MahasiswaCtrl', function ($scope) {
+  	$scope.mahasiswa;
+
     $scope.daftarMahasiswa = [
       {
       	id: 1,
       	npm: '012345',
       	nama: 'Endy Muhardin',
-      	tanggalLahir: '08-17-1945',
+      	tanggalLahir: '17-August-1945',
       	email: 'endy.muhardin@gmail.com'
       },
       {
-      	id: 1,
-      	npm: '012345',
-      	nama: 'Endy Muhardin',
-      	tanggalLahir: '08-17-1945',
-      	email: 'endy.muhardin@gmail.com'
-      },
-      {
-      	id: 1,
-      	npm: '012345',
-      	nama: 'Endy Muhardin',
-      	tanggalLahir: '08-17-1945',
-      	email: 'endy.muhardin@gmail.com'
-      },
-      {
-      	id: 1,
-      	npm: '012345',
-      	nama: 'Endy Muhardin',
-      	tanggalLahir: '08-17-1945',
-      	email: 'endy.muhardin@gmail.com'
+      	id: 2,
+      	npm: '54321',
+      	nama: 'Adi Sulistiono',
+      	tanggalLahir: '28-September-1928',
+      	email: 'adi.sulistiono@gmail.com'
       }
     ];
+
+    $scope.edit = function(x){
+    	$scope.mahasiswa = x;
+    };
   });
