487c9dd49faa3187bed009a63a9f3a55e8e7e688
Endy Muhardin <endy.muhardin@gmail.com>
2014-06-05 20:26:59 +0700

    aplikasi yeoman

---
+++ sesi-08/sample-code/client-side/app/scripts/app.js
@@ -0,0 +1,27 @@
+'use strict';
+
+angular
+  .module('clientSideApp', [
+    'ngCookies',
+    'ngResource',
+    'ngSanitize',
+    'ngRoute'
+  ])
+  .config(function ($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'views/main.html',
+        controller: 'MainCtrl'
+      })
+      .when('/about', {
+        templateUrl: 'views/about.html',
+        controller: 'AboutCtrl'
+      })
+      .when('/mahasiswa', {
+        templateUrl: 'views/mahasiswa.html',
+        controller: 'MahasiswaCtrl'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+  });
